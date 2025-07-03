527752c0b95cbde360716f199b000e0e158089fc
dudlf <dudlf016@gmail.com>
2025-07-03 11:36:01 +0900

    refactor: main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,13 @@
-import './styles/global.scss';
-import './styles/global.css';
-
-import React from 'react';
-import App from './app';
 import { NextUIProvider } from '@nextui-org/react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './app';
+import './main.scss';
 
-// Ensure React is available in the global scope
-window.React = React;
-
-const container = document.querySelector('#root');
-const root = createRoot(container as HTMLElement);
-
-
-root.render(
-    <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
         <NextUIProvider>
             <App />
         </NextUIProvider>
-    </React.StrictMode>
-);
+    </StrictMode>,
+)
